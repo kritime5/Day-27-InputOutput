092281b01d123b4a9f12caae84e263d926d99e58
Kritika <kritsme963@gmail.com>
2022-12-18 06:05:31 -0800

    Added:Employee payroll service

---
+++ src/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+    private final int id;
+    private final String name;
+    private final double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+}
